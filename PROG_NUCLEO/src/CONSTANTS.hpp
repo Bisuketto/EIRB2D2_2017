464875ff8b1c884a2a3e28d6e9ebb039e53ceae9
Hugues ALMORIN <hugues.almorin@enseirb-matmeca.fr>
2017-01-24 17:11:44 +0100

    Minor bug fixes

--- PROG_NUCLEO/src/CONSTANTS.hpp
+++ PROG_NUCLEO/src/CONSTANTS.hpp
@@ -7,5 +7,7 @@
 #define RAPPORT_ROUX 2.1428
 #define VITESSE_MAX 37782
 #define KP 0.707107
-#define PERIODE_ASSERV (1./256) //Intervalle d'échantillonnage à 256Hz
+#define KD 0
+#define KI 0
+#define PERIODE_ASSERV (1./512) //Intervalle d'échantillonnage à 256Hz
 #define PERIODE_PWM 25 //µs
--- PROG_NUCLEO/src/Motor.cpp
+++ PROG_NUCLEO/src/Motor.cpp
@@ -8,10 +8,10 @@ Motor::Motor() {
 	motorG = new PwmOut(PIN_PWMG);
 	motorD->period_us(PERIODE_PWM);
 	motorG->period_us(PERIODE_PWM);
+	motorD->write(0);
+	motorG->write(0);
 	sens_mD = new DigitalOut(PIN_SENSMD);
 	sens_mG = new DigitalOut(PIN_SENSMG);
-	dist_d = 0;
-	dist_g = 0;
 }
 
 Motor::Motor(Serial *pc_out) //Ajouter les pins dans les paramètres de constructeur
@@ -26,22 +26,32 @@ Motor::Motor(Serial *pc_out) //Ajouter les pins dans les paramètres de construct
 	motorG = new PwmOut(PIN_PWMG);
 	motorD->period_us(PERIODE_PWM);
 	motorG->period_us(PERIODE_PWM);
+	motorD->write(0);
+	motorG->write(0);
 	sens_mD = new DigitalOut(PIN_SENSMD);
 	sens_mG = new DigitalOut(PIN_SENSMG);
 	bouton = new DigitalIn(USER_BUTTON);
-	dist_d = 0;
-	dist_g = 0;
 }
 
-//Vitesse en mm/s
 void Motor::asserv_vitesse(float vitesse) {
 	consigne_vitesse = (vitesse / PERIMETER) * RESOLUTION;
 	routineAsserv->attach(callback(this, &Motor::routine), PERIODE_ASSERV); //256Hz
-	calc_sens(consigne_vitesse, consigne_vitesse);
 	while (1) {
 		if (modeTest)
 			test_encodeurs();
 	}
+}
+
+void Motor::asserv_position(float distance) {
+	consigne_position = (distance / PERIMETER) * RESOLUTION;
+	calc_sens(consigne_position, consigne_position);
+}
+
+void Motor::asserv_angle(float angle){
+	float envergure = 300; // In mm
+	float distance_roue = (angle * 3.14 / 180)*envergure;
+	float consigne_angle = (distance_roue / PERIMETER) * RESOLUTION;
+	calc_sens(consigne_angle, -consigne_angle); //Angle > 0 Rotate counterclockwise
 }
 
 void Motor::routine()
@@ -49,22 +59,22 @@ void Motor::routine()
 	calc_vitesse();
 	float epsilon_d = consigne_vitesse - vitesse_d;
 	float epsilon_g = consigne_vitesse - vitesse_g;
-	float delta_ed = epsilon_dant - epsilon_d;
-	float delta_eg = epsilon_gant - epsilon_g;
-	sum_epsilon_d += epsilon_d;
-	sum_epsilon_g += epsilon_g;
-	epsilon_dant = epsilon_d;
-	epsilon_gant = epsilon_g;
-	pwmd_eff = (epsilon_d * KP) / VITESSE_MAX;
-	pwmg_eff = (epsilon_g * KP) / VITESSE_MAX;
+	float delta_ed = epsilon_v_dant - epsilon_d;
+	float delta_eg = epsilon_v_gant - epsilon_g;
+	sum_epsilon_v_d += epsilon_d;
+	sum_epsilon_v_g += epsilon_g;
+	epsilon_v_dant = epsilon_d;
+	epsilon_v_gant = epsilon_g;
+	pwmd = (epsilon_d * KP + delta_ed * KD + sum_epsilon_v_d * KI) / VITESSE_MAX;
+	pwmg = (epsilon_g * KP + delta_eg * KD + sum_epsilon_v_g * KI) / VITESSE_MAX;
 
-	pwmd_eff = (pwmd_eff < 0) ? 0 : pwmd_eff;
-	pwmd_eff = (pwmd_eff > 1) ? 1 : pwmd_eff;
-	pwmg_eff = (pwmg_eff < 0) ? 0 : pwmg_eff;
-	pwmg_eff = (pwmg_eff > 1) ? 1 : pwmg_eff;
+	pwmd = (pwmd < 0) ? 0 : pwmd;
+	pwmd = (pwmd > 1) ? 1 : pwmd;
+	pwmg = (pwmg < 0) ? 0 : pwmg;
+	pwmg = (pwmg > 1) ? 1 : pwmg;
 
-	motorD->write(pwmd_eff);
-	motorG->write(pwmg_eff);
+	motorD->write(pwmd);
+	motorG->write(pwmg);
 }
 
 void Motor::calc_sens(float vd, float vg)
@@ -107,12 +117,13 @@ void Motor::test_encodeurs()
 	while (bouton->read() == 1);
 	while (bouton->read() == 0) {
 		if (affichage_debug->read_ms() >= 1000) {
+			pc->printf("PWMD : %f PWMG : %f\n", pwmd, pwmg);
 			//pc->printf("Imp G: %d\nImp D : %d\n", instEncoder->getImpEncG(), instEncoder->getImpEncD());
 			pc->printf("e G : %f e D : %f\n", consigne_vitesse - vitesse_g, consigne_vitesse - vitesse_d);
 			//pc->printf("corr G : %f corr D : %f\n", (consigne_vitesse - vitesse_g)*KP, (consigne_vitesse - vitesse_d)*KP);
 			pc->printf("Vitesse G : %f Vitesse D : %f\n", vitesse_g * PERIMETER / RESOLUTION, vitesse_d * PERIMETER / RESOLUTION);
 			//pc->printf("Vitesse G : %f inc/s\nVitesse D : %f inc/s\n\n", vitesse_d, vitesse_g);
-			pc->printf("Inc G : %d\nInc D : %d\n\n", instEncoder->getImpEncG(), instEncoder->getImpEncD());
+			//pc->printf("Inc G : %d\nInc D : %d\n\n", instEncoder->getImpEncG(), instEncoder->getImpEncD());
 			affichage_debug->reset();
 		}
 	}
